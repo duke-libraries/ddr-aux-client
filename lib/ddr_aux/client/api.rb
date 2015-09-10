a705bea6c3c58da1e4db4f3d8cb11b13d14e7a72
David Chandek-Stark <dchandekstark@gmail.com>
2015-09-10 13:43:18 -0400

    Version 1.0.0.rc2
    Fixed require in bin/console.
    Moved license methods.

--- lib/ddr_aux/client/api.rb
+++ lib/ddr_aux/client/api.rb
@@ -1,14 +1,28 @@
 module DdrAux::Client
   module Api
 
-    def license(id)
-      require_relative "license"
-      License.call(id)
+    def license(arg)
+      if arg.respond_to?(:to_h)
+        find_license(**(arg.to_h))
+      else
+        get_license(arg)
+      end
     end
 
-    def licenses(params=nil)
-      require_relative "licenses"
-      Licenses.call(params)
+    def get_license(id)
+      require_relative "api/get_license"
+      GetLicense.call(id)
+    end
+
+    def get_licenses
+      require_relative "api/get_licenses"
+      GetLicenses.call
+    end
+    alias_method :licenses, :get_licenses
+
+    def find_license(**args)
+      require_relative "api/find_license"
+      FindLicense.call(**args)
     end
 
   end
